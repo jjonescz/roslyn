5500ed545f35cec5520e0718e958cde2c2b95d62
Jan Jones <janjones@microsoft.com>
2023-02-16 15:45:42 +0100

    Embed the source generator and add asserts

--- src/Compilers/CSharp/Test/Semantic/SourceGeneration/Bug.cs
+++ src/Compilers/CSharp/Test/Semantic/SourceGeneration/Bug.cs
@@ -10,71 +10,19 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Microsoft.CodeAnalysis.Text;
+using Roslyn.Test.Utilities.TestGenerators;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests.SourceGeneration
 {
-    public class NpgsqlEfSqlGenerator : IIncrementalGenerator
+    public class SyntaxTreeChangeTests : CSharpTestBase
     {
-        public virtual void Initialize(IncrementalGeneratorInitializationContext context)
-        {
-            // CreateSyntaxProvider: this will hit the IntComparer
-            IncrementalValuesProvider<int> createSyntaxProviderOutputValues =
-                    context.SyntaxProvider
-                           .CreateSyntaxProvider(
-                               // simple filter for attributes
-                               predicate: static (node, _) =>
-                               {
-                                   return node is ClassDeclarationSyntax { Identifier.ValueText: "TestDependentClass" };
-                               },
-                               // filter out attributed classes that we don't care about
-                               transform: static (syntaxContext, _) =>
-                               {
-                                   return (syntaxContext.Node as ClassDeclarationSyntax)?.Identifier.ValueText.Length ?? 33;
-                               })
-                           .WithComparer(new IntComparer());
-
-            // this will NOT hit the IntComparer
-            IncrementalValuesProvider<int> forAttributeWithMetadataNameOutputValues =
-                    context.SyntaxProvider
-                           .ForAttributeWithMetadataName(
-                               "GenerateSql",
-                               // simple filter for attributes
-                               predicate: static (node, _) =>
-                               {
-                                   return node is ClassDeclarationSyntax { Identifier.ValueText: "TestDependentClass" };
-                               },
-                               // filter out attributed classes that we don't care about
-                               transform: static (syntaxContext, _) =>
-                               {
-                                   return (syntaxContext.TargetNode as ClassDeclarationSyntax)?.Identifier.ValueText.Length ?? 33;
-                               }).WithComparer(new IntComparer());
-
-            context.RegisterSourceOutput(createSyntaxProviderOutputValues, (SourceProductionContext sourceProductionContext, int value) =>
-            {
-                // ONLY EXECUTED ONCE
-                // generate the source code and add it to the output
-                string result = $"public class s_{value} {{ }}";
-                sourceProductionContext.AddSource($"CreateSyntaxProviderOutputValues_TEST_STRING_{(value)}.g.cs", SourceText.From(result, Encoding.UTF8));
-            });
-
-            context.RegisterSourceOutput(forAttributeWithMetadataNameOutputValues, (SourceProductionContext sourceProductionContext, int value) =>
-            {
-                // EXECUTED TWICE
-                // generate the source code and add it to the output
-                string result = $"public class s_{value} {{ }}";
-                sourceProductionContext.AddSource($"ForAttributeWithMetadataNameOutputValues_{(value)}.g.cs", SourceText.From(result, Encoding.UTF8));
-            });
-        }
         internal class IntComparer : IEqualityComparer<int>
         {
             public bool Equals(int x, int y) => x == y;
             public int GetHashCode(int obj) => obj.GetHashCode();
         }
-    }
 
-    public class SyntaxTreeChangeTests : CSharpTestBase
-    {
         [Fact]
         public void NoRelevantChangeTest()
         {
@@ -124,7 +72,62 @@ namespace Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests.SourceGeneration
                 CSharpSyntaxTree.ParseText(testClassSource.original, path: "./TestClass.cs"),
                 CSharpSyntaxTree.ParseText(testDependentClassSource, path: "./TestDependentClass.cs")
             });
-            var generator = new NpgsqlEfSqlGenerator();
+
+            var counter1 = 0;
+            var counter2 = 0;
+
+            var generator = new PipelineCallbackGenerator(context =>
+            {
+                // CreateSyntaxProvider: this will hit the IntComparer
+                IncrementalValuesProvider<int> createSyntaxProviderOutputValues =
+                        context.SyntaxProvider
+                               .CreateSyntaxProvider(
+                                   // simple filter for attributes
+                                   predicate: static (node, _) =>
+                                   {
+                                       return node is ClassDeclarationSyntax { Identifier.ValueText: "TestDependentClass" };
+                                   },
+                                   // filter out attributed classes that we don't care about
+                                   transform: static (syntaxContext, _) =>
+                                   {
+                                       return (syntaxContext.Node as ClassDeclarationSyntax)?.Identifier.ValueText.Length ?? 33;
+                                   })
+                               .WithComparer(new IntComparer());
+
+                // this will NOT hit the IntComparer
+                IncrementalValuesProvider<int> forAttributeWithMetadataNameOutputValues =
+                        context.SyntaxProvider
+                               .ForAttributeWithMetadataName(
+                                   "GenerateSql",
+                                   // simple filter for attributes
+                                   predicate: static (node, _) =>
+                                   {
+                                       return node is ClassDeclarationSyntax { Identifier.ValueText: "TestDependentClass" };
+                                   },
+                                   // filter out attributed classes that we don't care about
+                                   transform: static (syntaxContext, _) =>
+                                   {
+                                       return (syntaxContext.TargetNode as ClassDeclarationSyntax)?.Identifier.ValueText.Length ?? 33;
+                                   }).WithComparer(new IntComparer());
+
+                context.RegisterSourceOutput(createSyntaxProviderOutputValues, (SourceProductionContext sourceProductionContext, int value) =>
+                {
+                    // ONLY EXECUTED ONCE
+                    counter1++;
+                    // generate the source code and add it to the output
+                    string result = $"public class s_{value} {{ }}";
+                    sourceProductionContext.AddSource($"CreateSyntaxProviderOutputValues_TEST_STRING_{(value)}.g.cs", SourceText.From(result, Encoding.UTF8));
+                });
+
+                context.RegisterSourceOutput(forAttributeWithMetadataNameOutputValues, (SourceProductionContext sourceProductionContext, int value) =>
+                {
+                    // EXECUTED TWICE
+                    counter2++;
+                    // generate the source code and add it to the output
+                    string result = $"public class s_{value} {{ }}";
+                    sourceProductionContext.AddSource($"ForAttributeWithMetadataNameOutputValues_{(value)}.g.cs", SourceText.From(result, Encoding.UTF8));
+                });
+            });
 
             GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
 
@@ -134,6 +137,9 @@ namespace Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests.SourceGeneration
             compilation = compilation.ReplaceSyntaxTree(compilation.SyntaxTrees[0], CSharpSyntaxTree.ParseText(testClassSource.changed, path: "./TestClass.cs"));
             driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out outputCompilation, out diagnostics);
             Assert.Empty(diagnostics);
+
+            Assert.Equal(1, counter1);
+            Assert.Equal(1, counter2);
         }
     }
 }
