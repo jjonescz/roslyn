130541f3aeba610c6eed8f19857f1801c2ddd932
Jan Jones <janjones@microsoft.com>
2023-06-26 15:06:59 +0200

    Update more value check error tests

--- src/Compilers/CSharp/Test/Semantic/Semantics/OverloadResolutionTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/OverloadResolutionTests.cs
@@ -9518,16 +9518,31 @@ public static class Program
                 }
                 """;
 
-            var expectedDiagnostics = new[]
-            {
-                // (10,20): error CS0199: A static readonly field cannot be used as a ref or out value (except in a static constructor)
+            CreateCompilation(code, parseOptions: TestOptions.Regular11).VerifyDiagnostics(
+                // (10,20): error CS1615: Argument 1 may not be passed with the 'ref' keyword
                 //         Method(ref x);
-                Diagnostic(ErrorCode.ERR_RefReadonlyStatic, "x").WithLocation(10, 20)
-            };
+                Diagnostic(ErrorCode.ERR_BadArgExtraRef, "x").WithArguments("1", "ref").WithLocation(10, 20));
 
-            CreateCompilation(code, parseOptions: TestOptions.Regular11).VerifyDiagnostics(expectedDiagnostics);
-            CreateCompilation(code, parseOptions: TestOptions.RegularNext).VerifyDiagnostics(expectedDiagnostics);
-            CreateCompilation(code).VerifyDiagnostics(expectedDiagnostics);
+            verify(TestOptions.RegularNext);
+            verify();
+
+            void verify(CSharpParseOptions parseOptions = null)
+            {
+                var verifier = CompileAndVerify(code, expectedOutput: "5", parseOptions: parseOptions, verify: Verification.Fails);
+                verifier.VerifyDiagnostics(
+                    // (10,20): warning CS9502: The 'ref' modifier for argument 1 corresponding to 'in' parameter is equivalent to 'in'. Consider using 'in' instead.
+                    //         Method(ref x);
+                    Diagnostic(ErrorCode.WRN_BadArgRef, "x").WithArguments("1").WithLocation(10, 20));
+                verifier.VerifyIL("Program.Main", """
+                    {
+                      // Code size       11 (0xb)
+                      .maxstack  1
+                      IL_0000:  ldsflda    "int Program.x"
+                      IL_0005:  call       "void Program.Method(in int)"
+                      IL_000a:  ret
+                    }
+                    """);
+            }
         }
 
         [Fact]
