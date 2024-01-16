1914d046ec9d88118ac5ed8a3637b0f88c2cd56f
Jan Jones <janjones@microsoft.com>
2024-01-16 15:41:33 +0100

    Add a test

---
+++ src/Compilers/CSharp/Test/Emit2/Semantics/LockTests.cs
@@ -0,0 +1,99 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Semantics;
+
+public class LockTests : CSharpTestBase
+{
+    private const string LockTypeDefinition = """
+        namespace System.Threading
+        {
+            public class Lock
+            {
+                public Scope EnterLockScope() => new Scope();
+
+                public ref struct Scope
+                {
+                    public void Dispose() { }
+                }
+            }
+        }
+        """;
+
+    [Fact]
+    public void LockVsUsing()
+    {
+        var source = """
+            using System;
+            using System.Threading;
+
+            static class C
+            {
+                static readonly Lock _lock = new();
+
+                static void Main()
+                {
+                    M1();
+                    M2();
+                }
+
+                static void M1()
+                {
+                    Console.Write("1");
+                    lock (_lock)
+                    {
+                        Console.Write("2");
+                    }
+                    Console.Write("3");
+                }
+
+                static void M2()
+                {
+                    Console.Write("1");
+                    using (_lock.EnterLockScope())
+                    {
+                        Console.Write("2");
+                    }
+                    Console.Write("3");
+                }
+            }
+            """;
+        var verifier = CompileAndVerify(new[] { source, LockTypeDefinition }, expectedOutput: "123123",
+            verify: Verification.FailsILVerify);
+        verifier.VerifyDiagnostics();
+        var il = """
+            {
+              // Code size       52 (0x34)
+              .maxstack  1
+              .locals init (System.Threading.Lock.Scope V_0)
+              IL_0000:  ldstr      "1"
+              IL_0005:  call       "void System.Console.Write(string)"
+              IL_000a:  ldsfld     "System.Threading.Lock C._lock"
+              IL_000f:  callvirt   "System.Threading.Lock.Scope System.Threading.Lock.EnterLockScope()"
+              IL_0014:  stloc.0
+              .try
+              {
+                IL_0015:  ldstr      "2"
+                IL_001a:  call       "void System.Console.Write(string)"
+                IL_001f:  leave.s    IL_0029
+              }
+              finally
+              {
+                IL_0021:  ldloca.s   V_0
+                IL_0023:  call       "void System.Threading.Lock.Scope.Dispose()"
+                IL_0028:  endfinally
+              }
+              IL_0029:  ldstr      "3"
+              IL_002e:  call       "void System.Console.Write(string)"
+              IL_0033:  ret
+            }
+            """;
+        verifier.VerifyIL("C.M2", il);
+        verifier.VerifyIL("C.M1", il);
+    }
+}
