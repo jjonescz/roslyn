1c9cb3688fc94167a9ca25c89fd871c3f00c248f
Jan Jones <janjones@microsoft.com>
2024-11-19 16:02:09 +0100

    Remove unnecessary `using`

--- src/Compilers/CSharp/Portable/CodeGen/CodeGenerator.cs
+++ src/Compilers/CSharp/Portable/CodeGen/CodeGenerator.cs
@@ -14,7 +14,6 @@ using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Text;
-using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.CodeGen
 {
