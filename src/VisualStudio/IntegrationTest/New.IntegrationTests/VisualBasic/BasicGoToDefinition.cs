d5da43a46697bae561e3e7e9f8bb8af3f5223e86
Jan Jones <janjones@microsoft.com>
2025-01-07 16:37:02 +0100

    Unskip ObjectBrowserNavigation

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/VisualBasic/BasicGoToDefinition.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/VisualBasic/BasicGoToDefinition.cs
@@ -47,7 +47,7 @@ End Class", HangMitigatingCancellationToken);
         Assert.False(await TestServices.Shell.IsActiveTabProvisionalAsync(HangMitigatingCancellationToken));
     }
 
-    [IdeTheory(Skip = "https://github.com/dotnet/roslyn/issues/75458"), CombinatorialData]
+    [IdeTheory, CombinatorialData]
     public async Task ObjectBrowserNavigation(bool navigateToObjectBrowser)
     {
         var globalOptions = await TestServices.Shell.GetComponentModelServiceAsync<IGlobalOptionService>(HangMitigatingCancellationToken);
