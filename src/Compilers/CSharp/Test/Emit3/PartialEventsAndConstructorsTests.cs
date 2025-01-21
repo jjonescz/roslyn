d3832d7d257b4b84620e2fa271e07f938a9fa879
Jan Jones <janjones@microsoft.com>
2025-01-21 16:17:40 +0100

    Add some semantic tests

---
+++ src/Compilers/CSharp/Test/Emit3/PartialEventsAndConstructorsTests.cs
@@ -0,0 +1,87 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests;
+
+public sealed class PartialEventsAndConstructorsTests : CSharpTestBase
+{
+    [Theory, CombinatorialData]
+    public void Event_PartialLast([CombinatorialValues("", "public")] string modifier)
+    {
+        var source = $$"""
+            partial class C
+            {
+                {{modifier}}
+                partial event System.Action E;
+                {{modifier}}
+                partial event System.Action E { add { } remove { } }
+            }
+            """;
+        // PROTOTYPE: warning CS0067 should not be reported
+        CreateCompilation(source).VerifyDiagnostics(
+            // (4,33): warning CS0067: The event 'C.E' is never used
+            //     partial event System.Action E;
+            Diagnostic(ErrorCode.WRN_UnreferencedEvent, "E").WithArguments("C.E").WithLocation(4, 33));
+    }
+
+    [Fact]
+    public void Event_PartialNotLast()
+    {
+        var source = """
+            partial class C
+            {
+                partial public event System.Action E;
+                partial public event System.Action E { add { } remove { } }
+            }
+            """;
+        // PROTOTYPE: warning CS0067 should not be reported
+        CreateCompilation(source).VerifyDiagnostics(
+            // (3,5): error CS0267: The 'partial' modifier can only appear immediately before 'class', 'record', 'struct', 'interface', 'event', a constructor identifier, or a method or property return type.
+            //     partial public event System.Action E;
+            Diagnostic(ErrorCode.ERR_PartialMisplaced, "partial").WithLocation(3, 5),
+            // (3,40): warning CS0067: The event 'C.E' is never used
+            //     partial public event System.Action E;
+            Diagnostic(ErrorCode.WRN_UnreferencedEvent, "E").WithArguments("C.E").WithLocation(3, 40),
+            // (4,5): error CS0267: The 'partial' modifier can only appear immediately before 'class', 'record', 'struct', 'interface', 'event', a constructor identifier, or a method or property return type.
+            //     partial public event System.Action E { add { } remove { } }
+            Diagnostic(ErrorCode.ERR_PartialMisplaced, "partial").WithLocation(4, 5));
+    }
+
+    [Theory, CombinatorialData]
+    public void Constructor_PartialLast([CombinatorialValues("", "public")] string modifier)
+    {
+        var source = $$"""
+            partial class C
+            {
+                {{modifier}}
+                partial C();
+                {{modifier}}
+                partial C() { }
+            }
+            """;
+        CreateCompilation(source).VerifyDiagnostics();
+    }
+
+    [Fact]
+    public void Constructor_PartialNotLast()
+    {
+        var source = """
+            partial class C
+            {
+                partial public C();
+                partial public C() { }
+            }
+            """;
+        CreateCompilation(source).VerifyDiagnostics(
+            // (3,5): error CS0267: The 'partial' modifier can only appear immediately before 'class', 'record', 'struct', 'interface', 'event', a constructor identifier, or a method or property return type.
+            //     partial public C();
+            Diagnostic(ErrorCode.ERR_PartialMisplaced, "partial").WithLocation(3, 5),
+            // (4,5): error CS0267: The 'partial' modifier can only appear immediately before 'class', 'record', 'struct', 'interface', 'event', a constructor identifier, or a method or property return type.
+            //     partial public C() { }
+            Diagnostic(ErrorCode.ERR_PartialMisplaced, "partial").WithLocation(4, 5));
+    }
+}
