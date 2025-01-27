03ce996440ee0bcb1091464c29642f96973c395d
Jan Jones <janjones@microsoft.com>
2025-01-27 13:42:10 -0800

    Extend tests and docs

--- src/Compilers/CSharp/Test/Emit3/PartialEventsAndConstructorsTests.cs
+++ src/Compilers/CSharp/Test/Emit3/PartialEventsAndConstructorsTests.cs
@@ -17,6 +17,50 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests;
 public sealed class PartialEventsAndConstructorsTests : CSharpTestBase
 {
     [Fact]
+    public void ReturningPartialType_LocalFunction_InMethod()
+    {
+        var source = """
+                class @partial
+                {
+                    static void Main()
+                    {
+                        System.Console.Write(F().GetType().Name);
+                        partial F() => new();
+                    }
+                }
+                """;
+        CompileAndVerify(source, parseOptions: TestOptions.Regular13, expectedOutput: "partial").VerifyDiagnostics();
+    }
+
+    [Fact]
+    public void ReturningPartialType_LocalFunction_TopLevel()
+    {
+        var source = """
+                System.Console.Write(F().GetType().Name);
+                partial F() => new();
+                class @partial;
+                """;
+        CompileAndVerify(source, parseOptions: TestOptions.Regular13, expectedOutput: "partial").VerifyDiagnostics();
+    }
+
+    [Fact]
+    public void ReturningPartialType_Method()
+    {
+        var source = """
+            class C
+            {
+                partial F() => new();
+                static void Main()
+                {
+                    System.Console.Write(new C().F().GetType().Name);
+                }
+            }
+            class @partial;
+            """;
+        CompileAndVerify(source, parseOptions: TestOptions.Regular13, expectedOutput: "partial").VerifyDiagnostics();
+    }
+
+    [Fact]
     public void LangVersion()
     {
         var source = """
