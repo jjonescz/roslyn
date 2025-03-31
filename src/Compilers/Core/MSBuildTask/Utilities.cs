7c96d9913ccb92ee1085ede095c4c0f1b5c7c8a2
Jan Jones <janjones@microsoft.com>
2025-03-31 13:26:41 +0200

    Remove binfx check

--- src/Compilers/Core/MSBuildTask/Utilities.cs
+++ src/Compilers/Core/MSBuildTask/Utilities.cs
@@ -183,9 +183,7 @@ namespace Microsoft.CodeAnalysis.BuildTasks
             }
 
             return RuntimeHostInfo.IsDesktopRuntime
-                ? Path.GetFileName(assemblyDirectory) is "binfx"
-                    ? Path.Combine(Path.GetDirectoryName(assemblyDirectory)!, "bincore", Path.ChangeExtension(toolFileName, ".dll"))
-                    : Path.Combine(assemblyDirectory, toolFileName)
+                ? Path.Combine(assemblyDirectory, toolFileName)
                 : Path.Combine(assemblyDirectory, "bincore", toolFileName);
         }
     }
