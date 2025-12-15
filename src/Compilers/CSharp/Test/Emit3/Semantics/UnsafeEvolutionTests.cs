a2aae87c43d039107ecaf7214ede7dad63354fc1
Jan Jones <janjones@microsoft.com>
2025-12-15 17:21:45 +0100

    Remove unnecessary langversion tests

--- src/Compilers/CSharp/Test/Emit3/Semantics/UnsafeEvolutionTests.cs
+++ src/Compilers/CSharp/Test/Emit3/Semantics/UnsafeEvolutionTests.cs
@@ -2901,38 +2901,13 @@ public sealed class UnsafeEvolutionTests : CompilingTestBase
             unsafe { c.M2(null); }
             """;
 
-        var expectedDiagnostics = new[]
-        {
-            // (3,1): error CS9503: 'C.M2(int*)' must be used in an unsafe context because it has pointers in its signature
-            // c.M2(null);
-            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "c.M2(null)").WithArguments($"C.M2({parameterType})").WithLocation(3, 1),
-        };
-
         CreateCompilation(source,
             [libRef],
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.RegularNext,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.Regular14,
             options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
             .VerifyDiagnostics(
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+            // (3,1): error CS9503: 'C.M2(int*)' must be used in an unsafe context because it has pointers in its signature
             // c.M2(null);
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.M2(null)").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.M2(null);
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.M2(null)").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,6): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.M2(null);
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "null").WithArguments("updated memory safety rules").WithLocation(3, 6));
+            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "c.M2(null)").WithArguments($"C.M2({parameterType})").WithLocation(3, 1));
 
         CompileAndVerify("""
             var c = new C();
@@ -2986,35 +2961,13 @@ public sealed class UnsafeEvolutionTests : CompilingTestBase
             unsafe { c.M2(null); }
             """;
 
-        var expectedDiagnostics = new[]
-        {
-            // (3,1): error CS9503: 'C.M2(string)' must be used in an unsafe context because it has pointers in its signature
-            // c.M2(null);
-            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "c.M2(null)").WithArguments("C.M2(string)").WithLocation(3, 1),
-        };
-
         CreateCompilation(source,
             [libRef],
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.RegularNext,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.Regular14,
             options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
             .VerifyDiagnostics(
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+            // (3,1): error CS9503: 'C.M2(string)' must be used in an unsafe context because it has pointers in its signature
             // c.M2(null);
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.M2(null)").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.M2(null);
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.M2(null)").WithArguments("updated memory safety rules").WithLocation(3, 1));
+            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "c.M2(null)").WithArguments("C.M2(string)").WithLocation(3, 1));
 
         CompileAndVerify("""
             var c = new C();
@@ -3061,41 +3014,13 @@ public sealed class UnsafeEvolutionTests : CompilingTestBase
             unsafe { new int*[0].M2(); }
             """;
 
-        var expectedDiagnostics = new[]
-        {
-            // (2,1): error CS9503: 'E.M2(int*[])' must be used in an unsafe context because it has pointers in its signature
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "new int*[0].M2()").WithArguments("E.M2(int*[])").WithLocation(2, 1),
-        };
-
         CreateCompilation(source,
             [libRef],
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.RegularNext,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.Regular14,
             options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
             .VerifyDiagnostics(
-            // (2,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+            // (2,1): error CS9503: 'E.M2(int*[])' must be used in an unsafe context because it has pointers in its signature
             // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0]").WithArguments("updated memory safety rules").WithLocation(2, 1),
-            // (2,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0].M2()").WithArguments("updated memory safety rules").WithLocation(2, 1),
-            // (2,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0].M2()").WithArguments("updated memory safety rules").WithLocation(2, 1),
-            // (2,5): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "int*").WithArguments("updated memory safety rules").WithLocation(2, 5));
+            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "new int*[0].M2()").WithArguments("E.M2(int*[])").WithLocation(2, 1));
 
         CompileAndVerify("""
             123.M1();
@@ -3155,41 +3080,13 @@ public sealed class UnsafeEvolutionTests : CompilingTestBase
             unsafe { new int*[0].M2(); }
             """;
 
-        var expectedDiagnostics = new[]
-        {
-            // (2,1): error CS9503: 'E.extension(int*[]).M2()' must be used in an unsafe context because it has pointers in its signature
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "new int*[0].M2()").WithArguments("E.extension(int*[]).M2()").WithLocation(2, 1),
-        };
-
         CreateCompilation(source,
             [libRef],
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.RegularNext,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.Regular14,
             options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
             .VerifyDiagnostics(
-            // (2,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+            // (2,1): error CS9503: 'E.extension(int*[]).M2()' must be used in an unsafe context because it has pointers in its signature
             // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0]").WithArguments("updated memory safety rules").WithLocation(2, 1),
-            // (2,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0].M2()").WithArguments("updated memory safety rules").WithLocation(2, 1),
-            // (2,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0].M2()").WithArguments("updated memory safety rules").WithLocation(2, 1),
-            // (2,5): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].M2();
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "int*").WithArguments("updated memory safety rules").WithLocation(2, 5));
+            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "new int*[0].M2()").WithArguments("E.extension(int*[]).M2()").WithLocation(2, 1));
 
         CompileAndVerify("""
             123.M1();
@@ -3245,47 +3142,16 @@ public sealed class UnsafeEvolutionTests : CompilingTestBase
             unsafe { c.P2 = c.P2; }
             """;
 
-        var expectedDiagnostics = new[]
-        {
+        CreateCompilation(source,
+            [libRef],
+            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
+            .VerifyDiagnostics(
             // (3,1): error CS9503: 'C.P2' must be used in an unsafe context because it has pointers in its signature
             // c.P2 = c.P2;
             Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "c.P2").WithArguments("C.P2").WithLocation(3, 1),
             // (3,8): error CS9503: 'C.P2' must be used in an unsafe context because it has pointers in its signature
             // c.P2 = c.P2;
-            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "c.P2").WithArguments("C.P2").WithLocation(3, 8),
-        };
-
-        CreateCompilation(source,
-            [libRef],
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.RegularNext,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.Regular14,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.P2 = c.P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.P2").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.P2 = c.P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.P2").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.P2 = c.P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.P2 = c.P2").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,8): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.P2 = c.P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.P2").WithArguments("updated memory safety rules").WithLocation(3, 8),
-            // (3,8): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // c.P2 = c.P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "c.P2").WithArguments("updated memory safety rules").WithLocation(3, 8));
+            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "c.P2").WithArguments("C.P2").WithLocation(3, 8));
 
         CompileAndVerify("""
             var c = new C();
@@ -3347,50 +3213,16 @@ public sealed class UnsafeEvolutionTests : CompilingTestBase
             unsafe { new int*[0].P2 = new int*[0].P2; }
             """;
 
-        var expectedDiagnostics = new[]
-        {
+        CreateCompilation(source,
+            [libRef],
+            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
+            .VerifyDiagnostics(
             // (3,1): error CS9503: 'E.extension(int*[]).P2' must be used in an unsafe context because it has pointers in its signature
             // new int*[0].P2 = new int*[0].P2;
             Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "new int*[0].P2").WithArguments("E.extension(int*[]).P2").WithLocation(3, 1),
             // (3,18): error CS9503: 'E.extension(int*[]).P2' must be used in an unsafe context because it has pointers in its signature
             // new int*[0].P2 = new int*[0].P2;
-            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "new int*[0].P2").WithArguments("E.extension(int*[]).P2").WithLocation(3, 18),
-        };
-
-        CreateCompilation(source,
-            [libRef],
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.RegularNext,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(expectedDiagnostics);
-
-        CreateCompilation(source,
-            [libRef],
-            parseOptions: TestOptions.Regular14,
-            options: TestOptions.UnsafeReleaseExe.WithUpdatedMemorySafetyRules())
-            .VerifyDiagnostics(
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].P2 = new int*[0].P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0]").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,1): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].P2 = new int*[0].P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0].P2").WithArguments("updated memory safety rules").WithLocation(3, 1),
-            // (3,5): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].P2 = new int*[0].P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "int*").WithArguments("updated memory safety rules").WithLocation(3, 5),
-            // (3,18): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].P2 = new int*[0].P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0]").WithArguments("updated memory safety rules").WithLocation(3, 18),
-            // (3,18): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].P2 = new int*[0].P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "new int*[0].P2").WithArguments("updated memory safety rules").WithLocation(3, 18),
-            // (3,22): error CS8652: The feature 'updated memory safety rules' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-            // new int*[0].P2 = new int*[0].P2;
-            Diagnostic(ErrorCode.ERR_FeatureInPreview, "int*").WithArguments("updated memory safety rules").WithLocation(3, 22));
+            Diagnostic(ErrorCode.ERR_UnsafeMemberOperationCompat, "new int*[0].P2").WithArguments("E.extension(int*[]).P2").WithLocation(3, 18));
 
         CompileAndVerify("""
             var x = 123;
